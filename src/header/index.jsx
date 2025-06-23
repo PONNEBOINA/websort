e2fb3f28e1bc77c6cc2fa049c3ff6e4c13ac0806
PONNEBOINA <vamshiyadav406@gmail.com>
2025-06-23 09:02:38 +0530

    first commit

--- src/header/index.jsx
+++ src/header/index.jsx
@@ -17,23 +17,25 @@ const Header = () => {
 
   return (
     <nav className="navbar">
-      <div className="first-part">
-        <img
-          src="https://res.cloudinary.com/dnxbl0xrb/image/upload/v1750572223/Rectangle_3_mwiasp.png"
-          alt="logo"
-          className="logo"
-        />
-        <div className="hamburger" onClick={toggleMenu}>
-          ☰
+      <div className="navbar-container">
+        <div className="logo-section">
+          <img
+            src="https://res.cloudinary.com/dnxbl0xrb/image/upload/v1750572223/Rectangle_3_mwiasp.png"
+            alt="logo"
+            className="logo"
+          />
+          <div className="hamburger" onClick={toggleMenu}>
+            ☰
+          </div>
         </div>
-      </div>
 
-      <div className={`second-part ${isOpen ? "open" : ""}`}>
-        <Link to="/" className="links" onClick={() => setIsOpen(false)}>Home</Link>
-        <Link to="/aboutUs" className="links" onClick={() => setIsOpen(false)}>About Us</Link>
-        <Link to="/projects" className="links" onClick={() => setIsOpen(false)}>Projects</Link>
-        <Link to="/career" className="links" onClick={() => setIsOpen(false)}>Career</Link>
-        <button type="button" onClick={contactSection}>Contact Us</button>
+        <div className={`nav-links ${isOpen ? "open" : ""}`}>
+          <Link to="/" onClick={() => setIsOpen(false)}>Home</Link>
+          <Link to="/aboutUs" onClick={() => setIsOpen(false)}>About Us</Link>
+          <Link to="/projects" onClick={() => setIsOpen(false)}>Projects</Link>
+          <Link to="/career" onClick={() => setIsOpen(false)}>Career</Link>
+          <button onClick={contactSection}>Contact Us</button>
+        </div>
       </div>
     </nav>
   );
